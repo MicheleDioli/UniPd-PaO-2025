0863f3ad50952541e7781d79eadaca0afa3fe067
MicheleDioli <micheledioli.dm@gmail.com>
2024-12-25 17:38:47 +0100

    qrc

--- src/Rivista.h
+++ src/Rivista.h
@@ -15,6 +15,12 @@ private:
 public:
     Rivista(std::string codice, std::string descrizione, std::string genere, std::string breveDescrizione, int anno, std::string editore, int pagine, std::string pubblicatore, int intervalloPubblicazione, int edizione);
     std::string getEditore();
+    int getPagine();
+    std::string getPubblicatore();
+    int getIntervalloPubblicazione();
+    int getEdizione();
+    int difficoltaLettura();
+    int velocitaLettura();
 };
 
 #endif //RIVISTA_H
